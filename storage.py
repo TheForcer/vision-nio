0fd0bfd703062c5b59504c1347a4621743c76f29
Felix <git@theforcer.de>
2019-11-12 17:42:36 +0100

    Update dependencies & refresh template python code

--- storage.py
+++ storage.py
@@ -48,33 +48,3 @@ class Storage(object):
         # Initialize a connection to the database
         self.conn = sqlite3.connect(self.db_path)
         self.cursor = self.conn.cursor()
-
-    def get_sync_token(self):
-        """Retrieve the next_batch token from the last sync response.
-
-        Used to sync without retrieving messages we've processed in the past
-
-        Returns:
-            A str containing the last sync token or None if one does not exist
-        """
-        self.cursor.execute("SELECT token FROM sync_token")
-        rows = self.cursor.fetchone()
-
-        if not rows:
-            return None
-
-        return rows[0]
-
-    def save_sync_token(self, token):
-        """Save a token from a sync response.
-
-        Can be retrieved later to sync from where we left off
-
-        Args:
-            token (str): A next_batch token as part of a sync response
-        """
-        self.cursor.execute(
-            "INSERT OR REPLACE INTO sync_token " "(dedupe_id, token) VALUES (1, ?)",
-            (token,),
-        )
-        self.conn.commit()
